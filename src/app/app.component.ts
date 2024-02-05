f779353850ba0563619dc95d3646aa306720cd02
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-02-05 09:56:31 -0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { response } from 'express';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ang-dictionary';
+
+  url = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+  word = 'hello';
+  searchWord = `${this.url}${this.word}` 
+
+  fetchData() {
+    this.word = 'hello';
+
+    fetch(this.searchWord)
+    .then((response) => response.json())
+    .then((data)=> console.log(data))
+  }
 }
