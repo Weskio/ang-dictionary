e24cd29ca5db34b25ce4251210e154bb875c8941
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-02-06 12:01:48 -0800

    error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { NgClass, NgFor, NgStyle } from '@angular/common';
+import { NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { response } from 'express';
@@ -6,23 +6,24 @@ import { response } from 'express';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgClass, NgFor, NgStyle],
+  imports: [RouterOutlet, NgClass, NgFor, NgStyle, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   isDarkMode = false;
   url = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
-  data: any | undefined;
+  data: any;
   phonetic?: string;
-  meaning?: [];
-  isWordEntered?: boolean;
-  //definition?: string;
-  partOfSpeech?: [];
-  synonyms?: [];
-  isDropdown?: boolean;
-  isDropRotated?: boolean;
+  meaning: string[] = [];
+  isWordEntered = false;
+  partOfSpeech: string[] = [];
+  synonyms: string[] = [];
+  isDropdown = false;
+  isDropRotated = false;
   selectedFont = 'first';
+  isThereError = false;
+  word?: string;
 
   toggleTheme() {
     this.isDarkMode = !this.isDarkMode;
@@ -33,27 +34,40 @@ export class AppComponent {
     this.isDropRotated = !this.isDropRotated;
   }
 
-  applyFont(font:string){
-    this.selectedFont = font
+  applyFont(font: string) {
+    this.selectedFont = font;
   }
 
   fetchData(text: string) {
-    this.isWordEntered = true;
     return fetch(`${this.url}${text}`)
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
       .then((data) => {
         console.log(data);
+        this.isWordEntered = true;
         this.data = data;
+        this.word = data[0].word;
         this.phonetic = data[0].phonetic;
         this.meaning = data[0].meanings[0].definitions.map(
           (definition: { definition: any }) => definition.definition
         );
-        this.synonyms = data[0].meanings[0].definitions.map(
-          (synonym: { synonyms: any }) => synonym.synonyms
+        this.synonyms = data[0].meanings[0].definitions.flatMap(
+          (definition: { synonyms: any }) => definition.synonyms || []
         );
-        this.partOfSpeech = data[0].meanings.partOfSpeech;
+        this.partOfSpeech = data[0].meanings.flatMap(
+          (meaning: { partOfSpeech: any }) => meaning.partOfSpeech || []
+        );
         console.log(this.partOfSpeech);
         console.log(this.meaning);
+      })
+      .catch((err) => {
+        console.log(err)
+        this.isThereError = true;
+        this.isWordEntered = false;
       });
   }
 }
