2845f55efafdd0627723c1571042ef2ac932bd9a
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-02-05 16:51:05 -0800

    final commit for day 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { NgClass } from '@angular/common';
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { response } from 'express';
@@ -6,27 +6,42 @@ import { response } from 'express';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgClass],
+  imports: [RouterOutlet, NgClass, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  isDarkMode = false
+  isDarkMode = false;
   url = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
-  word = 'hello';
-  searchWord = `${this.url}${this.word}` 
+  data: any | undefined;
+  phonetic?: string;
+  meaning?: [];
+  isWordEntered?: boolean
+  //definition?: string;
+  partOfSpeech?: [];
+  synonyms?: [];
 
-  toggleTheme(){
-    this.isDarkMode= !this.isDarkMode
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
   }
 
-  fetchData() {
-    this.word = 'hello';
-
-    fetch(this.searchWord)
-    .then((response) => response.json())
-    .then((data)=> console.log(data))
+  fetchData(text: string) {
+    this.isWordEntered=true
+    return fetch(`${this.url}${text}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        this.data = data;
+        this.phonetic = data[0].phonetic;
+        this.meaning = data[0].meanings[0].definitions.map(
+          (definition: { definition: any }) => definition.definition
+        );
+        this.synonyms = data[0].meanings[0].definitions.map(
+          (synonym: { synonyms: any }) => synonym.synonyms
+        );
+        this.partOfSpeech = data[0].meanings.partOfSpeech;
+        console.log(this.partOfSpeech);
+        console.log(this.meaning);
+      });
   }
-
- 
 }
